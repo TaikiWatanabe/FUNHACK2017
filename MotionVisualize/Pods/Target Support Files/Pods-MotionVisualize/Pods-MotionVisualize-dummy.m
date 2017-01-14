eaf85f8ab2839347826dca9b6fdc3905b8fa74ec
渡辺泰伎 <b1014147@fun.ac.jp>
2017-01-14 14:48:54 +0900

    [add]socketioを追加

---
+++ MotionVisualize/Pods/Target Support Files/Pods-MotionVisualize/Pods-MotionVisualize-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MotionVisualize : NSObject
+@end
+@implementation PodsDummy_Pods_MotionVisualize
+@end
---
+++ MotionVisualize/Pods/Target Support Files/Pods-MotionVisualizeTests/Pods-MotionVisualizeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MotionVisualizeTests : NSObject
+@end
+@implementation PodsDummy_Pods_MotionVisualizeTests
+@end
